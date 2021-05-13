27154b8bcd34cb67f69d68fc68ba19d71434349e
Kam S <singhkam07@gmail.com>
2021-05-13 13:48:10 -0400

    Update Checker

--- source/viewer/ui/app.py
+++ source/viewer/ui/app.py
@@ -1,5 +1,8 @@
 from typing import List
 
+import tkinter as tk
+from tkinter import messagebox
+
 import kivy
 #kivy.require('2.0.0')
 
@@ -7,6 +10,7 @@ from kivy.lang.builder import Builder
 from kivy.properties import ObjectProperty
 from kivy.app import App
 from kivy.uix.screenmanager import SlideTransition
+from kivy.network.urlrequest import UrlRequest
 
 import kivymd
 from kivymd.app import MDApp
@@ -39,6 +43,32 @@ SOURCES: List[source.Source] = [
 ]
 
 
+class Version:
+    def __init__(self, version: str):
+        self.big, self.small, self.patch = [ int(part) for part in version.split('.') ]
+
+    def __lt__(self, other: 'Version'):
+        if self.big == other.big:
+            if self.small == other.small:
+                return self.patch < other.patch
+            return self.small < other.small
+        return self.big < other.big
+    
+    def __gt__(self, other: 'Version'):
+        if self.big == other.big:
+            if self.small == other.small:
+                return self.patch > other.patch
+            return self.small > other.small
+        return self.big > other.big
+    
+    def __eq__(self, other: 'Version'):
+        return self.big == other.big and self.small == other.small and self.patch == other.patch
+
+
+_VERSION = Version('0.1.0')
+_VERSION_PAGE = 'https://kams04.github.io/KReader/'
+
+
 class KReader(MDApp):
     kv_file = MAIN_KV_FILE
 
@@ -63,7 +93,28 @@ class KReader(MDApp):
         self.home_screen = self.root.ids['home_screen']
         self.reader_screen = self.root.ids['reader_screen']
         self.source_screen = self.root.ids['source_screen']
+        UrlRequest(_VERSION_PAGE, on_success=self.check_version, on_error=lambda *args: print('Checking for updates failed'))
         return super().on_start()
+
+    def check_version(self, request: UrlRequest, result: str):
+        newest = Version(result)
+        if _VERSION == newest:
+            print('On latest version')
+            return
+        if _VERSION < newest:
+            title = 'Update'
+            message = 'Update available. Check the github repository.'
+        else:
+            title = 'Beta'
+            message = 'You are on a non release version. Errors may occur.'
+        root = tk.Tk()
+        root.attributes("-topmost", True)
+        root.withdraw()
+        messagebox.showinfo(title, message)
+        try:
+            root.destroy()
+        except:
+            pass
 
     def clamp(self, value, min_value, max_value):
         return max(min(value, max_value), min_value)
