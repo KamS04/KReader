a4df5ae45943c909fbec21c06e653f42eba20185
Kam S <singhkam07@gmail.com>
2021-09-12 10:44:56 -0400

    Added pallete file that has all colors

---
+++ source/kreader/ui/pallete.py
@@ -0,0 +1,11 @@
+from kivy.utils import get_color_from_hex
+
+class Pallete:
+    def __init__(self):
+        self.light_background = get_color_from_hex('#EEEEEE')
+        self.background_text = get_color_from_hex('#FFFFFF')
+        self.main = get_color_from_hex('#0094C6')
+        self.black = get_color_from_hex('#000000')
+        self.dark_background = get_color_from_hex('#001242')
+        self.accent = get_color_from_hex('#005E7C')
+        self.darker_background = get_color_from_hex('#000022')
