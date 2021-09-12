52887a20cb73d360124b53aba469f74e5d9c25b0
Kam S <singhkam07@gmail.com>
2021-09-12 10:46:54 -0400

    Created basic button class from Label, that gives more control over colors + Added ToggalableButton which allows for control over selection and clear

---
+++ source/kreader/ui/widgets/button.py
@@ -0,0 +1,47 @@
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.label import Label
+from kivy.properties import ListProperty, BooleanProperty
+from kivy.utils import get_color_from_hex
+
+from .background import Background
+
+class LabelButton(ButtonBehavior, Label, Background):
+    down_color = get_color_from_hex('#32a4ce')
+    normal_color = get_color_from_hex('#585858')
+    disabled_color = get_color_from_hex('#1a1a1a')
+
+    def __init__(self, *args, **kwargs):
+        super(LabelButton, self).__init__(*args, **kwargs)
+        self.on_disabled(self, self.disabled)
+
+    def on_state(self, instance, new_state):
+        if new_state == 'down':
+            self.background_color = self.down_color
+        else:
+            self.background_color = self.normal_color
+    
+    def on_disabled(self, instance, disabled):
+        if disabled:
+            self.background_color = self.disabled_color
+        else:
+            self.on_state(self, self.state)
+
+class TogglableButton(LabelButton):
+    selected_color = get_color_from_hex('#15699e')
+    selected = BooleanProperty(False)
+
+    def on_state(self, instance, new_state):
+        if not self.selected:
+            super(TogglableButton, self).on_state(instance, new_state)
+    
+    def on_release(self):
+        self.selected = True
+
+    def clear(self):
+        self.selected = False
+
+    def on_selected(self, instance, selected):
+        if selected:
+            self.background_color = self.selected_color
+        else:
+            self.background_color = self.normal_color
